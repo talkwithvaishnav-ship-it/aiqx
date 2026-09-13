415439ca11a967e1e4c3eb13e11083fb81b9137e
talkwithvaishnav-ship-it <talkwithvaishnav@gmail.com>
2026-09-13 21:29:42 +0530

    Update telethon_client.py

--- telethon_client.py
+++ telethon_client.py
@@ -2,7 +2,7 @@ from telethon import TelegramClient
 from config import API_ID, API_HASH
 
 client = TelegramClient(
-    "quotex_session_new",
+    "quotex_session_final",
     API_ID,
     API_HASH
 )
